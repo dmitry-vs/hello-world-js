d0c3e3ec3cba3c923e883d63ca988c61bbdbf1ba
dmitry-vs <dmitryselin@inbox.ru>
2025-09-17 20:16:14 +0300

    Initial commit: Hello World JavaScript project

---
+++ index.js
@@ -0,0 +1,7 @@
+// Hello World на JavaScript
+console.log("Hello World");
+
+// Дополнительные примеры вывода в консоль
+console.log("Привет, мир!");
+console.log("Это простой проект на JavaScript");
+console.log("Текст выводится в консоль с помощью console.log()");
